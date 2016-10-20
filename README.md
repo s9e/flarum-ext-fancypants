19182e109e0debb6fc9d3eba4f83bfd581afb9c7
JoshyPHP <s9e.dev@gmail.com>
2016-10-20 06:31:35 +0200

    Updated for newer Flarum versions

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-s9e\fancypants is a Flarum extension that provides enhanced typography.
+s9e/fancypants is a Flarum extension that provides enhanced typography.
 
 ### How to install
 
-[Download the latest release](https://github.com/s9e/flarum-ext-fancypants/releases/latest). Unzip the archive into the `extensions` directory of your Flarum installation. Then go to the admin panel and enable the extension.
+```
+composer require s9e/flarum-ext-fancypants
+```
+
+Install with Composer then go to the admin panel and enable the extension.
--- bootstrap.php
+++ bootstrap.php
@@ -2,35 +2,23 @@
 
 /**
 * @package   s9e\fancypants
-* @copyright Copyright (c) 2015 The s9e Authors
+* @copyright Copyright (c) 2015-2016 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Flarum\FancyPants;
 
-use Exception;
-use Flarum\Events\FormatterConfigurator;
-use Flarum\Support\Extension as BaseExtension;
+use Flarum\Event\ConfigureFormatter;
 use Illuminate\Events\Dispatcher;
 
-class Extension extends BaseExtension
+function subscribe(Dispatcher $events)
 {
-	public function listen(Dispatcher $events)
-	{
-		$events->subscribe(__NAMESPACE__ . '\\Listener');
-	}
-}
+	$events->listen(
+		ConfigureFormatter::class,
+		function (ConfigureFormatter $event)
+		{
+			$event->configurator->Fancypants;
+		}
+	);
+};
 
-class Listener
-{
-	public function subscribe(Dispatcher $events)
-	{
-		$events->listen('Flarum\\Events\\FormatterConfigurator', [$this, 'enableFancyPants']);
-	}
-
-	public function enableFancyPants(FormatterConfigurator $event)
-	{
-		$event->configurator->FancyPants;
-	}
-}
-
-return __NAMESPACE__ . '\\Extension';
+return __NAMESPACE__ . '\\subscribe';
