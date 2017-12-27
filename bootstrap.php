8162ce0587eae540649870ad2075e9e41742a4e2
JoshyPHP <s9e.dev@gmail.com>
2017-12-27 13:22:41 +0100

    Updated for Flarum Beta 8

--- bootstrap.php
+++ bootstrap.php
@@ -2,19 +2,19 @@
 
 /**
 * @package   s9e\fancypants
-* @copyright Copyright (c) 2015-2016 The s9e Authors
+* @copyright Copyright (c) 2015-2017 The s9e Authors
 * @license   http://www.opensource.org/licenses/mit-license.php The MIT License
 */
 namespace s9e\Flarum\FancyPants;
 
-use Flarum\Event\ConfigureFormatter;
+use Flarum\Formatter\Event\Configuring;
 use Illuminate\Events\Dispatcher;
 
 function subscribe(Dispatcher $events)
 {
 	$events->listen(
-		ConfigureFormatter::class,
-		function (ConfigureFormatter $event)
+		Configuring::class,
+		function (Configuring $event)
 		{
 			$event->configurator->FancyPants;
 		}
